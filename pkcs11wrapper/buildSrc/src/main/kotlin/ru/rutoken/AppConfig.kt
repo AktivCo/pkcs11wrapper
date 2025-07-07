21a664b53a361571445ee72a0b9a5d7bf8e623d6
Evgenii Potapov <potapov@rutoken.ru>
2025-07-07 18:17:07 +0300

    wrapper: Use public pkcs11jna version
    Up version to 4.3.1

--- pkcs11wrapper/buildSrc/src/main/kotlin/ru/rutoken/AppConfig.kt
+++ pkcs11wrapper/buildSrc/src/main/kotlin/ru/rutoken/AppConfig.kt
@@ -1,6 +1,6 @@
 object AppConfig {
     const val kotlinVersion = "1.6.20"
 
-    const val versionName = "4.3.0"
+    const val versionName = "4.3.1"
     const val wrapperKtxVersionName = "1.1.0"
 }
